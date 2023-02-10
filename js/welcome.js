006fcbadc2037d66ec311e1a78726029427499c2
SEYYl <1719099363@qq.com>
2023-02-10 10:16:00 +0800

    Site updated: 2023-02-10 10:15:59

---
+++ js/welcome.js
@@ -0,0 +1,98 @@
+//首次访问弹窗
+if (localStorage.getItem("popWelcomeWindow") != "0") {
+    if (document.referrer == undefined || document.referrer.indexOf("baijing.ml") != -1 || document.referrer.indexOf("sey-yl-github-io.vercel.app") != -1) { //改成自己域名，注意是referrer!!! qwq
+        Snackbar.show({
+            pos: "top-right",
+            showAction: false,
+            text: '欢迎访问本站！'
+        })
+    } else {
+        Snackbar.show({
+            pos: "top-right",
+            showAction: false,
+            text: `欢迎来自${document.referrer.split("://")[1].split("/")[0]}的童鞋访问本站！`
+        })
+        localStorage.setItem("popWelcomeWindow", "0");
+    }
+}
+if (sessionStorage.getItem("popCookieWindow") != "0") {
+    setTimeout(function () {
+        Snackbar.show({
+            text: '本站使用Cookie和本地/会话存储保证浏览体验和网站统计',
+            pos: 'bottom-right',
+            actionText: "查看博客声明",
+            onActionClick: function (element) {
+                window.open("/license")
+            },
+        })
+    }, 3000)
+}
+//不在弹出Cookie提醒
+sessionStorage.setItem("popCookieWindow", "0");
+
+//自带上文浏览器提示
+
+function browserTC() {
+    btf.snackbarShow("");
+    Snackbar.show({
+        text: '浏览器版本较低，网站样式可能错乱',
+        actionText: '关闭',
+        duration: '6000',
+        pos: 'bottom-right'
+    });
+}
+function browserVersion() {
+    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
+    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
+    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //Edge浏览器
+    var isFirefox = userAgent.indexOf("Firefox") > -1; //Firefox浏览器
+    var isOpera = userAgent.indexOf("Opera") > -1 || userAgent.indexOf("OPR") > -1; //Opera浏览器
+    var isChrome = userAgent.indexOf("Chrome") > -1 && userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Edge") == -1 && userAgent.indexOf("OPR") == -1; //Chrome浏览器
+    var isSafari = userAgent.indexOf("Safari") > -1 && userAgent.indexOf("Chrome") == -1 && userAgent.indexOf("Edge") == -1 && userAgent.indexOf("OPR") == -1; //Safari浏览器
+    if (isEdge) {
+        if (userAgent.split('Edge/')[1].split('.')[0] < 90) {
+            browserTC()
+        }
+    } else if (isFirefox) {
+        if (userAgent.split('Firefox/')[1].split('.')[0] < 90) {
+            browserTC()
+        }
+    } else if (isOpera) {
+        if (userAgent.split('OPR/')[1].split('.')[0] < 80) {
+            browserTC()
+        }
+    } else if (isChrome) {
+        if (userAgent.split('Chrome/')[1].split('.')[0] < 90) {
+            browserTC()
+        }
+    } else if (isSafari) {
+        //不知道Safari哪个版本是该淘汰的老旧版本
+    }
+}
+//2022-10-29修正了一个错误：过期时间应使用toGMTString()，而不是toUTCString()，否则实际过期时间在中国差了8小时
+function setCookies(obj, limitTime) {
+    let data = new Date(new Date().getTime() + limitTime * 24 * 60 * 60 * 1000).toGMTString()
+    for (let i in obj) {
+        document.cookie = i + '=' + obj[i] + ';expires=' + data
+    }
+}
+function getCookie(name) {
+    var arr, reg = new RegExp("(^| )" + name + "=([^;]*)(;|$)");
+    if (arr = document.cookie.match(reg))
+        return unescape(arr[2]);
+    else
+        return null;
+}
+if (getCookie('browsertc') != 1) {
+    setCookies({
+        browsertc: 1,
+    }, 1);
+    browserVersion();
+}
+
+
+// 作者: Ariasaka
+// 链接: https://yisous.xyz/posts/becc831a/
+// 来源: Ariasakaの小窝
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
